8d55daf271b2a7d07346257ea47a17866efdc8c1
deepadoddamani <deepadoddamani24@gmail.com>
2022-06-06 18:21:55 +0530

    modified app file by prakash

--- Application.java
+++ Application.java
@@ -6,5 +6,21 @@ class FactorialExample{
       fact=fact*i;    
   }    
   System.out.println("Factorial of "+number+" is: "+fact);    
- }  
+## changes done by prakash
+class Main {
+
+  public static void main(String[] args) {
+    
+    System.out.println("Enter two numbers");
+    int first = 10;
+    int second = 20;
+    
+    System.out.println(first + " " + second);
+
+    // add two numbers
+    int sum = first + second;
+    System.out.println("The sum is: " + sum);
+  }
+} 
+}  
 } 
